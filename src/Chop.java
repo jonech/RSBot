f0f2901e495683830342d0a62a8f3db6eaa4640e
jonech <jone@live.hk>
2015-02-10 21:53:07 +1100

    some uncompleted tutorial codes

---
+++ src/Chop.java
@@ -0,0 +1,34 @@
+import org.powerbot.script.rt6.ClientContext;
+import org.powerbot.script.rt6.GameObject;
+
+
+public class Chop extends Task<ClientContext>{
+
+	private int[] treeIds = {38616, 38627, 58006};
+	
+	public Chop(ClientContext ctx) {
+		super(ctx);
+	}
+
+	@Override
+	public boolean activate() {
+		
+		return (ctx.backpack.select().count() < 28) &&
+				(!ctx.objects.select().id(treeIds).isEmpty()) &&
+				(ctx.players.local().animation() == -1);
+	}
+
+	@Override
+	public void execute() {
+		GameObject tree = ctx.objects.nearest().poll();
+		
+		if (tree.inViewport()) {
+			tree.interact("Chop");
+		}
+		else {
+			ctx.movement.step(tree);
+			ctx.camera.turnTo(tree);
+		}
+	}
+
+}
---
+++ src/PowerChopper.java
@@ -0,0 +1,19 @@
+import org.powerbot.script.PollingScript;
+import org.powerbot.script.rt6.ClientContext;
+
+
+@Script.Manifest(name="chop'n'drop", description="cuts willow logs and drops just like the Powerbot tutorial")
+public class PowerChopper extends PollingScript<ClientContext>{
+	
+	@Override
+	public void start() {
+		
+	}
+
+	@Override
+	public void poll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
